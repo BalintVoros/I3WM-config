bfb40230e5566921644782e32bf253a37628a934
Vörös Bálint <91821323+BalintVoros@users.noreply.github.com>
2024-06-10 14:44:36 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 #Automated installer
-
-
-git clone https://github.com/BalintVoros/I3WM-config
-cd I3WM-config
-sudo cp i3/*  ~/.config/i3/
-sudo cp i3blocks/* ~/.config/i3blocks/
-sudo cp i3status/* ~/.config/i3status/
 mkdir /home/$USER/Wallpapers
+sudo cp i3/*  /home/$USER/.config/i3/
+sudo cp i3blocks/* /home/$USER/.config/i3blocks/
+sudo cp i3status/* /home/$USER/.config/i3status/
+
 sudo cp Wallpapers/* ~/home/$USER/Wallpapers
-sudo cp neofetch/* ~/.config/neofetch/
-sudo cp kitty/* ~/.config/kitty/
-sudo cp rofi/* ~/.config/rofi/
-sudo cp Neovim/* ~/.config/nvim/
+sudo cp neofetch/* /home/$USER/.config/neofetch/
+sudo cp kitty/* /home/$USER/.config/kitty/
+sudo cp rofi/* /home/$USER/.config/rofi/
+sudo cp Neovim/* ~/home/$USER/.config/nvim/
 mkdir /home/$USER/.config/scripts
 sudo cp scripts/* ~/.config/scripts/
 
