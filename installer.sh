a9e88f39c58316fab9af53349a5e8bb0c35ca414
Vörös Bálint <91821323+BalintVoros@users.noreply.github.com>
2024-06-10 22:47:23 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,16 +2,16 @@
 
 #Automated installer
 mkdir /home/$USER/Wallpapers
-sudo cp i3/*  /home/$USER/.config/i3/
-sudo cp i3blocks/* /home/$USER/.config/i3blocks/
-sudo cp i3status/* /home/$USER/.config/i3status/
+sudo cp -r i3/   /home/$USER/.config/i3/
+sudo cp  -r i3blocks/ /home/$USER/.config/i3blocks/
+sudo cp  -r i3status/ /home/$USER/.config/i3status/
 
-sudo cp Wallpapers/* ~/home/$USER/Wallpapers
-sudo cp neofetch/* /home/$USER/.config/neofetch/
-sudo cp kitty/* /home/$USER/.config/kitty/
-sudo cp rofi/* /home/$USER/.config/rofi/
-sudo cp Neovim/* ~/home/$USER/.config/nvim/
+sudo cp -r Wallpapers/ /home/$USER/Wallpapers
+sudo cp -r neofetch/ /home/$USER/.config/neofetch/
+sudo cp -r kitty/ /home/$USER/.config/kitty/
+sudo cp -r rofi/ /home/$USER/.config/rofi/
+sudo cp -r  Neovim/ ~/home/$USER/.config/nvim/
 mkdir /home/$USER/.config/scripts
-sudo cp scripts/* ~/.config/scripts/
+sudo cp -r  scripts/ ~/.config/scripts/
 
 echo "A Script lefutott köszönöm hogy használtad/The script successfully executed thanks for using it Author: BalintVoros"
