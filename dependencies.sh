7c1164264e8b04e1354e3c9762e11d75120a4504
Vörös Bálint <91821323+BalintVoros@users.noreply.github.com>
2024-06-16 21:02:33 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -12,16 +12,16 @@ makepkg -si
 cd ..
  
 sudo pacman -R i3lock
-sudo pacman -S acpi bluez-utils bluez blueman qbittorrent zsh lxappearance
+sudo pacman -S acpi bluez-utils bluez blueman qbittorrent zsh lxappearance yad thunar
 sudo pacman -S flameshot scrot feh picom kitty rofi libreoffice neovim neofetch
-sudo pacman -S pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack alsa-utils playerctl
-
+sudo pacman -S pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack alsa-utils playerctl yad
+sudo pacman -R xterm
 yay -S spotify
 yay -S i3lock-color
 sudo yay -S discord
 yay -S  brave-bin
 sudo cp -R capitaine-cursors /usr/share/icons
-sudo cp -R kora-gery /usr/share/icons
+sudo cp -R kora-grey /usr/share/icons
 sudo cp -R VALYRIAN-Total-Steel /usr/share/themes
 sudo cp -R fonts/0xProtoNerdFont-Regular.ttf /usr/share/fonts
 sudo cp -R fonts/0xProtoNerdFontMono-Regular.ttf /usr/share/fonts
@@ -31,4 +31,4 @@ cd .config/scripts
 sudo chmod +x *
 
 
-sudo sytemctl start bluetooth
+sudo systemctl start bluetooth
