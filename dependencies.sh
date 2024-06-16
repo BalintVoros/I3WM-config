344a029ab89df8dcc481f7266f039bd2b73700cd
Vörös Bálint <91821323+BalintVoros@users.noreply.github.com>
2024-06-16 21:06:11 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 
-#Dependencies installer
+#Automate fully installer
+
+sudo cp -r i3/   /home/$USER/.config/
+sudo cp  -r i3blocks/ /home/$USER/.config/
+sudo cp  -r i3status/ /home/$USER/.config/
+
+sudo cp -r Wallpapers/ /home/$USER/Wallpapers
+sudo cp -r neofetch/ /home/$USER/.config/
+sudo cp -r kitty/ /home/$USER/.config/
+sudo cp -r rofi/ /home/$USER/.config/
+sudo cp -r  scripts/ home/$USER/.config/
+
 
 sudo pacman -S git
 
@@ -26,9 +37,19 @@ sudo cp -R VALYRIAN-Total-Steel /usr/share/themes
 sudo cp -R fonts/0xProtoNerdFont-Regular.ttf /usr/share/fonts
 sudo cp -R fonts/0xProtoNerdFontMono-Regular.ttf /usr/share/fonts
 sudo cp -R fonts/0xProtoNerdFontPropo-Regular.ttf /usr/share/fonts
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
+
 cd ..
 cd .config/scripts
 sudo chmod +x *
 
 
 sudo systemctl start bluetooth
+
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
